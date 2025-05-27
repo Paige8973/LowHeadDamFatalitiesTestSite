899f1d0801854771823b9ea81d8f7b4936c3c4d2
Paige8973 <paige8973@gmail.com>
2025-05-27 16:17:02 -0600

    Update scripts.js New deployment for updated "last updated" date

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
         const DataService = (function() {
             // Private variables
             let damData = [];
-            const DATA_URL = "https://script.google.com/macros/s/AKfycbzk60MjFjrv9P-xtAtj2xSty0mjbTpYdJf-flTjmWP8oFbyIOA43xIOofMlr5OzUuwgMg/exec";
+            const DATA_URL = "https://script.google.com/macros/s/AKfycbxHBVUMNJ_I5QpcSia0c4pypcXApASE0Bz1lxRfL-lC7_MooKcV7Wbl2z4bm-a97me-bw/exec";
             const stateMap = {
                     'alabama': 'AL', 'alaska': 'AK', 'arizona': 'AZ', 'arkansas': 'AR',
                     'california': 'CA', 'colorado': 'CO', 'connecticut': 'CT', 'delaware': 'DE',
