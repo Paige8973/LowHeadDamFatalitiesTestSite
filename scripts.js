0d290f1ca6137800d29a5d73069d00bdbed114d8
Paige8973 <paige8973@gmail.com>
2025-07-28 14:10:14 -0600

    Update scripts.js new google script for verification

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@
             // Private variables
             let damData = [];
             let metaData = {};
-            const DATA_URL = "https://script.google.com/macros/s/AKfycbzcHGj4uH2fZB5JPNiRGYoc60Xt8w_aZobIkTnp2vdcEs9e5kRerFsmwqP0VW1VvP-KRQ/exec";
+            const DATA_URL = "https://script.google.com/macros/s/AKfycbyVY6b4508P7-WseObKIs-SXmdtldimdDQib09_F6CFikpYGiLeQRnjoT4dGkMHjUIn1w/exec";
             const stateMap = {
                     'alabama': 'AL', 'alaska': 'AK', 'arizona': 'AZ', 'arkansas': 'AR',
                     'california': 'CA', 'colorado': 'CO', 'connecticut': 'CT', 'delaware': 'DE',
